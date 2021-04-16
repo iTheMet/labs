1281526e47bade0bd885ad02907d180a8edcc83d
iTheMet <44820844+iTheMet@users.noreply.github.com>
2021-04-16 20:56:40 +0300

    koki

--- COCOMO/src/sample/Main.java
+++ COCOMO/src/sample/Main.java
@@ -19,7 +19,7 @@ public class Main /*extends Application*/ {
 
     public static void main(String[] args) {
         //launch(args);
-        Calculations.EAFsource();
+        Calculations.calc9();
         Calculations.ConsoleVisual();
     }
 }
